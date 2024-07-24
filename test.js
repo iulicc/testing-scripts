5acf6934d97491671cbb387aeb7a9f6236a20262
Iulian Ursu <141002129+iulicc@users.noreply.github.com>
2024-07-24 11:53:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 function changeTextFormat() {
-var valueOne = loader.engine.document.getElementById(114168733).getProperty('value.value');
 console.log("Value is:");
-console.log(valueOne);
 }
 window.onchange = changeTextFormat;
